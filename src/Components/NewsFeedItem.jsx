945345adef3de4e41a6b157492eaf3cdcb0b077e
Courtney Hampton <jaye0888@gmail.com>
2023-01-27 15:09:58 +0100

    Update NewsFeedItem.jsx

--- src/Components/NewsFeedItem.jsx
+++ src/Components/NewsFeedItem.jsx
@@ -23,7 +23,7 @@ const NewsFeedItem = ({ post }) => {
       setliked(true);
     }
   };
-  console.log("THIS IS WHAT YOU CAME FOR: " + post.user.image);
+  console.log("THIS IS WHAT YOU CAME FOR: " + post.post.user.image);
   return (
     <>
       <div className="news-feed-post mt-3">
