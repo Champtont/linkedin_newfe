8dde5011e4e5e24b5a31147f7f6ae4d3a3fbac0a
Courtney Hampton <jaye0888@gmail.com>
2023-01-27 16:55:33 +0100

    Update NewsFeedItem.jsx

--- src/Components/NewsFeedItem.jsx
+++ src/Components/NewsFeedItem.jsx
@@ -8,7 +8,7 @@ import { IoIosSend } from "react-icons/io";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
-const NewsFeedItem = ({ post }) => {
+const NewsFeedItem = ({ posts }) => {
   const likes = Math.floor(Math.random() * 200);
 
   const [Likes, setLikes] = useState(likes);
@@ -23,16 +23,16 @@ const NewsFeedItem = ({ post }) => {
       setliked(true);
     }
   };
-  console.log("THIS IS WHAT YOU CAME FOR: " + post.user.image);
+  console.log("THIS IS WHAT YOU CAME FOR: " + posts.user.image);
   return (
     <>
-      <div className="news-feed-post mt-3">
+      <div className="news-feed-posts mt-3">
         <Row>
           <div>
             <Link to={"/profile"}>
               <img
-                className="news-post-profile-image ml-2"
-                src={post.user.image}
+                className="news-posts-profile-image ml-2"
+                src={posts.user.image}
               />
             </Link>
           </div>
@@ -41,14 +41,14 @@ const NewsFeedItem = ({ post }) => {
             <div>
               <small>
                 <strong>
-                  {post.user.name} {post.user.surname}
+                  {posts.user.name} {posts.user.surname}
                 </strong>
               </small>{" "}
               <BsDot className="mt-1" /> <small>3rd+</small>
             </div>
             <div>
-              <small>{post.user.username}</small> <BsDash className="mt-1" />{" "}
-              <small>{post.user.title}</small>
+              <small>{posts.user.username}</small> <BsDash className="mt-1" />{" "}
+              <small>{posts.user.title}</small>
             </div>
           </div>
         </Row>
@@ -57,13 +57,13 @@ const NewsFeedItem = ({ post }) => {
             className="d-flex flex-column px-2 pb-2"
             /*style={{ paddingLeft: ".5em", paddingRight: ".5em" }}*/
           >
-            {post.text}
+            {posts.text}
           </div>
           <div style={{ width: "100%" }}>
             <img
               className="mb-3 img-fluid"
               src={
-                post.image ? `${post.image}` : "https://picsum.photos/505/300"
+                posts.image ? `${posts.image}` : "https://picsum.photos/505/300"
               }
               style={{ width: "100%", height: "100%", objectFit: "cover" }}
             />
@@ -79,18 +79,18 @@ const NewsFeedItem = ({ post }) => {
               </a>
             </div>
           </div>
-          <hr className="bottom-post-divider" />
-          <div className="post-action-buttons d-flex justify-content-between w-100 mx-3">
-            <span className="footer-icons-new-post" onClick={handleLikes}>
+          <hr className="bottom-posts-divider" />
+          <div className="posts-action-buttons d-flex justify-content-between w-100 mx-3">
+            <span className="footer-icons-new-posts" onClick={handleLikes}>
               {liked ? <AiFillLike /> : <AiOutlineLike />} Like
             </span>
-            <span className="footer-icons-new-post">
+            <span className="footer-icons-new-posts">
               <FaRegCommentDots /> Comment
             </span>
-            <span className="footer-icons-new-post">
-              <BiRepost /> Repost
+            <span className="footer-icons-new-posts">
+              <BiRepost /> Reposts
             </span>
-            <span className="footer-icons-new-post">
+            <span className="footer-icons-new-posts">
               <IoIosSend /> Send
             </span>
           </div>
