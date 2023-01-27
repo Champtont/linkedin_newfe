b0f85ce0ad3874c9d2c0267e844040f0e5c84392
Courtney Hampton <jaye0888@gmail.com>
2023-01-27 13:52:11 +0100

    trying something

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -15,9 +15,6 @@ const mainReducer = combineReducers({
 const persistConfig = {
   key: "root",
   storage: localStorage,
-  transforms: [
-    encryptTransform({ secretKey: process.env.REACT_APP_PRIVACY_KEY }),
-  ],
 };
 
 const peristedReducer = persistReducer(persistConfig, mainReducer);
